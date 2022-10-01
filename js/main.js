8c5d900e9a96d0ecc6b31aa1031fc07316c0644e
Airi Tomizawa <loveriruri633@gmail.com>
2022-10-01 10:39:53 +0900

    js

--- js/main.js
+++ js/main.js
@@ -7,6 +7,57 @@ $(function($) {
     });
 
 
+//     // スクロールして表示領域に入ったらclass付与
+//     $(".js-fadeRight").on("inview", function () {
+//         $(this).addClass("is-inview");
+//       });
+
+
+
+
+//       //.box要素が画面に入った時〜
+// $(".box").on("inview", function () {
+//     //その.boxにクラスfadeInRight追加
+//     $(this).addClass("fadeInRight");
+
+//     if(isInView) {
+//         $(this).stop().addClass("fadeInRight");
+//     } else {
+//         $(this).stop().removeClass("fadeInRight");
+//     }
+//   });
+
+
+//     $(".inview_re").on("inview", function (event, isInView) {
+//       if (isInView) {
+//         $(this).stop().addClass("is-show");
+//       } else {
+//         $(this).stop().removeClass("is-show");
+//       }
+//     });
+//   });
+
+//.box要素が画面に入った時〜
+$(".boxRight").on("inview", function (event, isInView) {
+    //その.boxにクラスfadeInRight追加
+    if (isInView) {
+    $(this).stop().addClass("fadeInRight");
+} else {
+    $(this).stop().removeClass("fadeInRight");
+}
+  });
+
+  //.box要素が画面に入った時〜
+$(".boxLeft").on("inview", function (event, isInView) {
+    //その.boxにクラスfadeInRight追加
+    if (isInView) {
+    $(this).stop().addClass("fadeInLeft");
+} else {
+    $(this).stop().removeClass("fadeInLeft");
+}
+  });
+
+
     
 $(".slide-items").slick({
     autoplay:true,
